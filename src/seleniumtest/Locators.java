8fb60922ffdcd401bc80343e8158a77ba0fbb2ff
Ind20 <indresh.maurya@gmail.com>
2023-05-06 17:05:56 +0530

    css locators and wait method

--- src/seleniumtest/Locators.java
+++ src/seleniumtest/Locators.java
@@ -1,5 +1,7 @@
 package seleniumtest;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,11 +12,12 @@ public class Locators {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/Asus/javaprojects/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice");
 		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
 		driver.findElement(By.name("inputPassword")).sendKeys("hello12234");
 		driver.findElement(By.className("signInBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		
 		//driver.close();
 	}
