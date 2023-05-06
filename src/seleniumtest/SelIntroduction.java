cff6c38f4c82a556e7cdfa658b3c3dc31a513a82
Ind20 <indresh.maurya@gmail.com>
2023-05-06 16:13:40 +0530

    visit url and close browser in firefox

--- src/seleniumtest/SelIntroduction.java
+++ src/seleniumtest/SelIntroduction.java
@@ -1,18 +1,18 @@
 package seleniumtest;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
 
 public class SelIntroduction {
 	
 	public static void main(String[] args) {
 		//System.setProperty("webdriver.chrome.driver", "/Users/Asus/javaprojects/chromedriver.exe");
-		//System.setProperty("webdriver.gecko.driver", "/Users/Asus/javaprojects/geckodriver.exe");
-		System.setProperty("webdriver.edge.driver", "/Users/Asus/javaprojects/msedgedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "/Users/Asus/javaprojects/geckodriver.exe");
+		//System.setProperty("webdriver.edge.driver", "/Users/Asus/javaprojects/msedgedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		//WebDriver driver = new EdgeDriver();
-		//WebDriver driver = new EdgeDriver();
-		WebDriver driver = new EdgeDriver();
 		driver.get("https://www.google.com/");
 		System.out.println(driver.getCurrentUrl());
 		driver.close();
