297d28b09bcb52de53b557cc10f6a0bc3177e9d2
Ind20 <indresh.maurya@gmail.com>
2023-05-06 15:54:39 +0530

    visit url and close browser in edge

--- src/seleniumtest/SelIntroduction.java
+++ src/seleniumtest/SelIntroduction.java
@@ -1,12 +1,18 @@
 package seleniumtest;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class SelIntroduction {
 	
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "/Users/Asus/javaprojects/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "/Users/Asus/javaprojects/chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "/Users/Asus/javaprojects/geckodriver.exe");
+		System.setProperty("webdriver.edge.driver", "/Users/Asus/javaprojects/msedgedriver.exe");
+		//WebDriver driver = new EdgeDriver();
+		//WebDriver driver = new EdgeDriver();
+		WebDriver driver = new EdgeDriver();
 		driver.get("https://www.google.com/");
 		System.out.println(driver.getCurrentUrl());
 		driver.close();
