11dabe673b63ca0d4cb7c98210a6ea98adea5a93
Ind20 <indresh.maurya@gmail.com>
2023-05-06 16:42:59 +0530

    using locators submit a form

--- src/seleniumtest/SelIntroduction.java
+++ src/seleniumtest/SelIntroduction.java
@@ -1,21 +1,17 @@
 package seleniumtest;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SelIntroduction {
 	
 	public static void main(String[] args) {
-		//System.setProperty("webdriver.chrome.driver", "/Users/Asus/javaprojects/chromedriver.exe");
-		System.setProperty("webdriver.gecko.driver", "/Users/Asus/javaprojects/geckodriver.exe");
-		//System.setProperty("webdriver.edge.driver", "/Users/Asus/javaprojects/msedgedriver.exe");
-		//WebDriver driver = new ChromeDriver();
-		WebDriver driver = new FirefoxDriver();
-		//WebDriver driver = new EdgeDriver();
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/Asus/javaprojects/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
 		driver.get("https://www.google.com/");
 		System.out.println(driver.getCurrentUrl());
-		driver.close();
+		//driver.close();
 	}
 
 }
