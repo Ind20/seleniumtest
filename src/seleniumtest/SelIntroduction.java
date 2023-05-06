f64bcc98a60902ce9e739e684fd379e08ec56d9f
Ind20 <indresh.maurya@gmail.com>
2023-05-06 15:27:06 +0530

    visit url and close browser

--- src/seleniumtest/SelIntroduction.java
+++ src/seleniumtest/SelIntroduction.java
@@ -7,6 +7,9 @@ public class SelIntroduction {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "/Users/Asus/javaprojects/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
 	}
 
 }
