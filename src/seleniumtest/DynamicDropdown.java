ceac67c6eb1aa95e2b2d3bc659a960c2486847af
Ind20 <indresh.maurya@gmail.com>
2023-05-07 18:13:03 +0530

    updating dynamic dropdown

---
+++ src/seleniumtest/DynamicDropdown.java
@@ -0,0 +1,27 @@
+package seleniumtest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropdown {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/Asus/javaprojects/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		Thread.sleep(1000);
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//a[@value='MAA']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.id("ctl00_mainContent_ddl_destinationStation1_CTXT")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("(//a[@value='BLR'])[2]")).click();
+	    //driver.findElement(By.id("btnclosepaxoption")).click();
+	    //System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+	}
+
+}
