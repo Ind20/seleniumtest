77c8dae8ddc1da3bf3ce017885de796c11671e71
Ind20 <indresh.maurya@gmail.com>
2023-05-07 09:10:36 +0530

    assert method to validate string

--- src/seleniumtest/Locators.java
+++ src/seleniumtest/Locators.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/Asus/javaprojects/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -17,14 +17,22 @@ public class Locators {
 		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
 		driver.findElement(By.name("inputPassword")).sendKeys("hello12234");
 		driver.findElement(By.className("signInBtn")).click();
-		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		driver.findElement(By.linkText("Forgot your password?")).click();
 		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
 		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("John@gmail.com");
-		driver.findElement(By.cssSelector("input[placeholder='Email']")).clear();
 		driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("1234567890");
-		
-		//driver.close();
+		Thread.sleep(2000);
+		driver.findElement(By.className("reset-pwd-btn")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.className("go-to-login-btn")).click();
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		Thread.sleep(2000);
+		driver.findElement(By.className("signInBtn")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[text()='Log Out']")).click();
+		Thread.sleep(2000);
+		driver.close();
 	}
 
 }
