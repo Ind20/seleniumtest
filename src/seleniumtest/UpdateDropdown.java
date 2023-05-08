c7817fce55d838ac777a55c478b436299c61f06c
Ind20 <indresh.maurya@gmail.com>
2023-05-08 10:12:20 +0530

    Validating test by assertEquals

--- src/seleniumtest/UpdateDropdown.java
+++ src/seleniumtest/UpdateDropdown.java
@@ -3,6 +3,7 @@ package seleniumtest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class UpdateDropdown {
 	
@@ -25,7 +26,8 @@ public class UpdateDropdown {
 			i++;
 		}
 	    
-	    driver.findElement(By.id("btnclosepaxoption")).click();
+		driver.findElement(By.id("btnclosepaxoption")).click();
+	    Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
 	    System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 	}
 
