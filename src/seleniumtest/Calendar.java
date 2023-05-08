fb15c0eec094fa79d9d5bb4b009bbf93b40e8607
Ind20 <indresh.maurya@gmail.com>
2023-05-08 11:06:31 +0530

    end to end test cs

--- src/seleniumtest/Calendar.java
+++ src/seleniumtest/Calendar.java
@@ -25,19 +25,6 @@ public class Calendar {
 		Thread.sleep(1000);
 		driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight")).click();
 		
-		driver.findElement(By.id("divpaxinfo")).click();
-		Thread.sleep(1000);
-		
-		int i = 0;
-		
-		while(i<4)
-		{
-			driver.findElement(By.id("hrefIncAdt")).click();
-			Thread.sleep(1000);
-			i++;
-		}
-		Thread.sleep(1000);
-		driver.findElement(By.id("ctl00_mainContent_btn_FindFlights")).click();
 		
 		System.out.println("Test Completed Successfully");
 	}
